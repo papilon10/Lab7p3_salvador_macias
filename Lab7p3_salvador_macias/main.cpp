802a6f5728a3b846a7fb52552200e0e641d13570
papilon10 <salvigna17@gmail.com>
2025-09-05 13:12:52 -0600

    Initial Commit

---
+++ Lab7p3_salvador_macias/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab7p3_salvador_macias
+//
+//  Created by salvador macias on 05/09/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
